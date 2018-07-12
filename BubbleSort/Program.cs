4c9056451293a05f7f59a44431ab15c491b5bcb8
gerosantacruz <geronimo.santacruz2017@gmail.com>
2018-07-12 20:23:02 -0300

    ok

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -13,7 +13,9 @@ namespace BubbleSort
             Console.WriteLine("Before: 5, 6, 1, 3, 7, 2, 4, 8, 9");
 
             //use this to know when to stop sorting
-            bool swapped;
+            bool swapped; 
+
+            // here we use a do loop (or you coudl use a while loop
 
 
         }
