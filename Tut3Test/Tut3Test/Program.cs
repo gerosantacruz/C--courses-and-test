5d911f6a05aeedd6399c29b5430eedad61866341
gerosantacruz <geronimo.santacruz2017@gmail.com>
2018-07-19 18:11:21 -0300

    edx

---
+++ Tut3Test/Tut3Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tut3Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Array.Sort(a);
+            int kth = 1;
+            int smallest = a[0];
+            foreach (int num in a)
+            {
+                if (num != smallest)
+                {
+                    kth++;
+                }
+                if (kth == k)
+                {
+                    return num;
+                }
+            }
+            // return -1 if not found
+            return -1;
+
+
+        }
+    }
+}
