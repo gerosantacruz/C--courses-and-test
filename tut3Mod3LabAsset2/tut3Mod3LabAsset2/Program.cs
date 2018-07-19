13e90f41b30dd02510337de8d0eaed480362d399
gerosantacruz <geronimo.santacruz2017@gmail.com>
2018-07-19 19:38:30 -0300

    New lab assetment done

---
+++ tut3Mod3LabAsset2/tut3Mod3LabAsset2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace tut3Mod3LabAsset2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList studentList = new ArrayList();
+
+            Student student1 = new Student("juan", "Perez", 5);
+            Student student2 = new Student("Laura", "Ramirez", 7);
+            Student student3 = new Student("Fabio", "Mendoza", 17);
+
+            Random rand = new Random();
+
+            for (int i= 0; i< 5; i++)
+            {
+                student1.Addgrade(Math.Round(rand.NextDouble() * 20 + 80, 2));
+                student2.Addgrade(Math.Round(rand.NextDouble() * 20 + 80, 2));
+                student3.Addgrade(Math.Round(rand.NextDouble() * 20 + 80, 2));
+            }
+
+            studentList.Add(student1);
+            studentList.Add(student2);
+            studentList.Add(student3);
+
+            Console.WriteLine("Student in student list: \n");
+
+            foreach(Student student in studentList)
+            {
+                Console.WriteLine($"{student.Name} {student.LastName} with id of {student.Id}");
+                Console.Write("Grades: ");
+
+                foreach(double grade in student.Grades)
+                {
+                    Console.Write($"{grade}, ");
+                }
+
+                Console.WriteLine("\n");
+            }
+
+        }
+    }
+}
---
+++ tut3Mod3LabAsset2/tut3Mod3LabAsset2/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tut3Mod3LabAsset2
+{
+    class Student
+    {
+        private string name;
+        private string lastName;
+        private int id;
+
+        private Stack grades = new Stack();
+
+        public string Name { get => name; set => name = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public int Id { get => id; set => id = value; }
+        public Stack Grades { get => grades; }
+
+        public Student(string name, string lastName, int Id)
+        {
+            Name = name;
+            LastName = lastName;
+            id = Id;
+        }
+
+        public void Addgrade(double grade)
+        {
+            Grades.Push(grade);
+        }
+    }
+}
